87afe762c7230b0ac5f7c7c839a5689d9756512d
ChromatixZero <beaumontdominic@hotmail.com>
2024-12-13 19:52:29 -0500

    Javalove

--- scripts/main.js
+++ scripts/main.js
@@ -15,23 +15,23 @@ let logBtn = document.querySelector("button");
 let title = document.querySelector("h1");
 
 function setUserName() {
-    const userName = prompt("Como te llamas?");
-    if (!userName) {
-        setUserName();
-    } else {
-        localStorage.setItem("name", userName);
-        //title.textContent = `Bienvenido, monsieul ${userName}`;
-        title.textContent = "Bienvenido, monsieul " + userName;
-    }
+  const userName = prompt("Como te llamas?");
+  if (!userName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", userName);
+    //title.textContent = `Bienvenido, monsieul ${userName}`;
+    title.textContent = "Bienvenido, monsieul " + userName;
+  }
 }
 
 if (!localStorage.getItem("name")) {
-    //setUserName();
+  //setUserName();
 } else {
-    const storedName = localStorage.getItem("name");
-    title.textContent = "Bienvenido, monsieul " + storedName;
+  const storedName = localStorage.getItem("name");
+  title.textContent = "Bienvenido, monsieul " + storedName;
 }
 
 logBtn.onclick = () => {
-    setUserName();
-}
+  setUserName();
+};
